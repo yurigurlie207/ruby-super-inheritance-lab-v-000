b5bc6b40ff114d2b5b0044306f8a899e8faa3280
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 02:12:22 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,5 +5,6 @@ class ChattyStudent
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
-  d
+  def raise_hand
+    en
 end
