25e976fa0fc64135af1a685a91a9c444d792da5c
Adrienne Yao <adrienne.yao@icloud.com>
2018-05-08 02:10:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,11 @@
 class Student
 
 def hello
-  puts "Hello there! I'm so excited to learn stuff"
+  puts "Hello there! I'm so excited to learn stuff."
+end
+
+def raise_hand
+  puts "Pick me!"
 end
 
 end
